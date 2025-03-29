7c0e062c838cc9cb59da538e3fcede4309419401
natalie-ib <nata.i07@mail.ru>
2025-03-30 01:49:47 +0400

    mr

---
+++ frontend/scripts/index.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const swiper = new Swiper('.principle-swiper', {
+        loop: true,
+        slidesPerView: 1,
+        spaceBetween: 10,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+});
